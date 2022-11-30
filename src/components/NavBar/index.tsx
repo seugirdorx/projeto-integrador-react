8c304bfeb629ce2765e49aa267331068efa2c14c
DanielSanCo <daniel.santos3210@outlook.com>
2022-11-30 09:14:31 -0300

    Inicio do site criando o componente NavBar

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,52 @@
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import React from 'react';
+import './style.css';
+
+
+const Navbar = () => {
+    
+    return(
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor' >
+                        <Typography variant="h5" color="inherit">
+                            GeneratEdu
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                postagens
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                logout
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+}
+
+export default Navbar;
